5d243fd7e9633b9e0443f4bec2e62ae9ea4f849d
Laharii07 <132770768+Laharii07@users.noreply.github.com>
2026-03-19 05:07:24 +0530

    Enhance comments in revenue calculation SQL query
    Updated comments for clarity and added explanations about revenue calculation.

--- Queries/01. Calculate Total Revenue Generated.sql
+++ Queries/01. Calculate Total Revenue Generated.sql
@@ -1,10 +1,29 @@
-/* what is revenue ?
+/* What is revenue?
 Total income a business generates from selling goods or services before deducting any expenses 
 Core formula:
-           Revenue = Number of Units sold * Price per unit
+          Gross Revenue: The total sales before any deductions
+                     Gross Revenue = Total units sold * average price per unit
+          Net revenue: The actual revenue earned after accounting for deductions
+                     Gross Revenue -(Returns + Discounts + Allowances) 
+- Goods returned by customers, reducing gross revenue    
+- Reductions in price offered to customers, such as early payment discounts, will also affect 
+- Allowance is a reduction in price due to minor product defects
+- Money received for services not yet delivered is recorded as a liability, not revenue, until the work is done
+** Revenue vs Profit ** 
+Revenue is the total money in, while profit(bottom line) is what remains after all expenses, taxes, and 
+Costs are deducted from revenue
+
+Price is the cost of a good, and freight value is the total monetary cost charged for transporting goods.
+including shipping rates, surcharges and insurance
+*/
 
 
--- Question 1 Calculate total revenue generated in the dataset. -- 
+-- Question 1: Calculate the total revenue generated in the dataset. -- 
 
-select sum(price+freight_value) as revenue 
-from order_items
+SELECT SUM(price+freight_value) AS Revenue 
+FROM order_items
+
+/* Calculating revenue helps to evaluate business performance and profitability.
+for performance evaluation, customer segmentation, sales and product analysis, financial forecasting ,
+pricing strategy optimisations
+*/
