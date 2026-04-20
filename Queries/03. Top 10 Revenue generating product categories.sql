f1f3fcd170b6b5765b1f098cf1a82dd4e3014860
Laharii07 <132770768+Laharii07@users.noreply.github.com>
2026-04-20 10:53:26 +0530

    Add SQL query for top 10 revenue categories
    Added SQL query to retrieve top 10 revenue generating product categories with analysis comments.

---
+++ Queries/03. Top 10 Revenue generating product categories.sql
@@ -0,0 +1,11 @@
+-- Top 10 Revenue Generating Product Categories
+Select 
+  Top 10 sum(oi.price + oi.freight_value) as Revenue , 
+  p.product_category_name
+from order_items oi
+join products p on oi.product_id = p.product_id
+group by p.product_category_name
+order by revenue desc
+
+-- Analyzing the top 10 revenue-generating product categories is a fundamental business strategy used to maximize profitability, optimize resources, and drive strategic growth.
+-- By focusing on the categories that drive the majority of income, businesses can gain actionable insights into customer demand, streamline operations, and make informed decisions about product mix, pricing, and marketing, often relying on the principle that a small percentage of products often generates the majority of sales (similar to the 80/20 rule)
